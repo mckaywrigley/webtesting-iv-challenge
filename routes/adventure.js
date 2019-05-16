e1a5fcb6cf749fa87b75edd6dd3df1673010a4a1
Mckay <mckaywrigley@gmail.com>
2019-05-16 07:21:20 -1000

    creates DELETE adventure endpoint

--- routes/adventure.js
+++ routes/adventure.js
@@ -2,10 +2,15 @@ const express = require("express");
 const router = express.Router();
 
 router.post("/", (req, res) => {
-  adventure
-    .save(req.body)
+  Adventure.save(req.body)
     .then(adventure => res.status(201).json(adventure))
-    .catch(err => res.status(500).json(err));
+    .catch(err => res.status(400).json(err));
+});
+
+router.delete("/:id", (req, res) => {
+  Adventure.delete(req.params.id)
+    .then(adventure => res.status(200).json(adventure))
+    .catch(err => res.status(404).json(err));
 });
 
 module.exports = router;
