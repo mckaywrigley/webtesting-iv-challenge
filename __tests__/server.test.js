1ab46582544ff9e1660cebb5f7895873524da5d7
Mckay <mckaywrigley@gmail.com>
2019-05-16 07:08:57 -1000

    tests delete adventure by id

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -21,14 +21,28 @@ describe("Server tests.", () => {
       expect(res.status).toBe(201);
     });
 
-    it('responds with 400 if adventure is invalid', () => {
-        const adventure = {};
+    it("responds with 400 if adventure is invalid", async () => {
+      const adventure = {};
 
-        const res = await request(server)
+      const res = await request(server)
         .post("/adventure")
         .send(adventure);
 
       expect(res.status).toBe(400);
-    })
+    });
+  });
+
+  describe("DELETE /adventure/:id", () => {
+    it("deletes adventure with id and responds with 200", async () => {
+      const res = await request(server).delete("/adventure/1");
+
+      expect(res.status).toBe(200);
+    });
+
+    it("responds with 404 if id not provided or found", async () => {
+      const res = await request(server).delete("/adventure/100");
+
+      expect(res.status).toBe(404);
+    });
   });
 });
