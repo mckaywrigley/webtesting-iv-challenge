6a9babe983d050169db6805640a1e212ef3dc3ce
Mckay <mckaywrigley@gmail.com>
2019-05-16 07:04:25 -1000

    tests for creation of adventure

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -9,4 +9,26 @@ describe("Server tests.", () => {
         .expect(200);
     });
   });
+
+  describe("POST /adventures", () => {
+    it("creates adventure and responds with 201", async () => {
+      const adventure = { name: "Scuba Diving" };
+
+      const res = await request(server)
+        .post("/adventure")
+        .send(adventure);
+
+      expect(res.status).toBe(201);
+    });
+
+    it('responds with 400 if adventure is invalid', () => {
+        const adventure = {};
+
+        const res = await request(server)
+        .post("/adventure")
+        .send(adventure);
+
+      expect(res.status).toBe(400);
+    })
+  });
 });
